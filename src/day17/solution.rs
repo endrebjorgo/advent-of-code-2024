16a0d375014f1efcd0bb916856b510b4d369b228
endrebjorgo <endrebjorgo@gmail.com>
2024-12-23 03:07:49 +0100

    Day 17: Remove OpType enum

--- src/day17/solution.rs
+++ src/day17/solution.rs
@@ -3,39 +3,9 @@ use std::fs::File;
 use std::time::Instant;
 
 #[derive(Debug)]
-enum OpType {
-    ADV = 0,
-    BXL,
-    BST,
-    JNZ,
-    BXC,
-    OUT,
-    BDV,
-    CDV,
-}
-
-impl OpType {
-    fn from_usize(value: usize) -> Option<OpType> {
-        match value {
-            0 => Some(OpType::ADV),
-            1 => Some(OpType::BXL),
-            2 => Some(OpType::BST),
-            3 => Some(OpType::JNZ),
-            4 => Some(OpType::BXC),
-            5 => Some(OpType::OUT),
-            6 => Some(OpType::BDV),
-            7 => Some(OpType::CDV),
-            _ => None,
-        }
-    }
-}
-
-type Program = Vec<(OpType, usize)>;
-
-#[derive(Debug)]
 struct Computer {
     ip: usize,
-    program: Program,
+    program: Vec<usize>,
     a_reg: usize,
     b_reg: usize,
     c_reg: usize,
@@ -60,20 +30,9 @@ impl Computer {
         assert!(buf_lines.next() == Some(""));
 
         let program_str = buf_lines.next().unwrap().split_once(": ").unwrap().1;
-        let program_vec: Vec<usize> = program_str.split(",")
+        let program: Vec<usize> = program_str.split(",")
             .map(|e| usize::from_str_radix(e, 10).unwrap()).collect();
         
-        let mut program = Program::new();
-        let mut idx = 0;
-        while idx < program_vec.len() {
-            if let Some(optype) = OpType::from_usize(program_vec[idx]) {
-                program.push((optype, program_vec[idx+1]));
-            } else {
-                panic!();
-            }
-            idx += 2;
-        }
-
         assert!(buf_lines.next() == None);
 
         Self { ip: 0, program, a_reg, b_reg, c_reg }
@@ -93,37 +52,40 @@ impl Computer {
         let mut output: Vec<usize> = Vec::new();
 
         while self.ip < self.program.len() {
-            match self.program[self.ip] {
-                (OpType::ADV, operand) => {
+            let optype = self.program[self.ip];
+            let operand = self.program[self.ip + 1];
+
+            match optype {
+                0 => {
                     self.a_reg /= usize::pow(2, self.combo(operand) as u32);
                 },
-                (OpType::BXL, operand) => {
+                1 => {
                     self.b_reg ^= operand;
                 },
-                (OpType::BST, operand) => {
+                2 => {
                     self.b_reg = self.combo(operand) % 8;
                 },
-                (OpType::JNZ, operand) => {
+                3 => {
                     if self.a_reg != 0 {
-                        assert!(operand % 2 == 0);
-                        self.ip = operand / 2;
+                        self.ip = operand;
                         continue;
                     }
                 },
-                (OpType::BXC, _) => {
+                4 => {
                     self.b_reg ^= self.c_reg;
                 },
-                (OpType::OUT, operand) => {
+                5 => {
                     output.push(self.combo(operand) % 8);
                 },
-                (OpType::BDV, operand) => {
+                6 => {
                     self.b_reg = self.a_reg / usize::pow(2, self.combo(operand) as u32);
                 },
-                (OpType::CDV, operand) => {
+                7 => {
                     self.c_reg = self.a_reg / usize::pow(2, self.combo(operand) as u32);
                 },
+                _ => unreachable!(),
             }
-            self.ip += 1;
+            self.ip += 2;
         }
         self.ip = 0;
         output
